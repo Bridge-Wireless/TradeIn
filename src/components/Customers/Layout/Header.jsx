f92bd7175f203803142ab166382e2ebdeb0c5d1c
amanpreet-bridge <amanpreet@bridge-wireless.com>
2024-10-21 15:58:05 -0400

    changes design

--- src/components/Customers/Layout/Header.jsx
+++ src/components/Customers/Layout/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <div className="sticky-top d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom box-shadow">
             <h5 className="my-0 mr-md-auto font-weight-normal"><Link to="/">Evergreen Wireless</Link></h5>
                 <Navigation />
-                <Link className="btn btn-outline-primary" to="/login">Login</Link>
+                <Link className="btn btn-outline-primary me-2" to="/login">Login</Link>
                 
                 <Link className="btn btn-outline-primary" to="/admin">Sales Rep</Link>
             </div>
