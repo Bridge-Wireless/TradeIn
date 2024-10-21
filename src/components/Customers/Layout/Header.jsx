e2ac96fa013a90fff786883d58d989ff31237f91
amanpreet-bridge <amanpreet@bridge-wireless.com>
2024-10-21 15:18:54 -0400

    change in adding sales rep Link

--- src/components/Customers/Layout/Header.jsx
+++ src/components/Customers/Layout/Header.jsx
@@ -9,6 +9,8 @@ const Header = () => {
             <h5 className="my-0 mr-md-auto font-weight-normal"><Link to="/">Evergreen Wireless</Link></h5>
                 <Navigation />
                 <Link className="btn btn-outline-primary" to="/login">Login</Link>
+                
+                <Link className="btn btn-outline-primary" to="/admin">Sales Rep</Link>
             </div>
         </>
     );
